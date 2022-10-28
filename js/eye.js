4788e52bd227b498effcaebadd1a7a54ae3e67c6
aulaul688 <aulaul688@gmail.com>
2022-10-29 00:30:23 +0800

    妝容修容全更新

--- js/eye.js
+++ js/eye.js
@@ -8,10 +8,7 @@ $.fn.type=function(){
     var index=$(this).index();
     //讓下面相應索引號類型形容顯示 其餘隱藏
     $(".one_type").eq(index).show().siblings().hide();
-    $(".other").css("top","250px");
-    $(".footer").css("top","250px")
     $.fn.sub(index);
-    $.fn.step(index);
 })
 }
 
@@ -19,26 +16,13 @@ $.fn.sub = function(index){
     $(".choose_btn").click(function(){
         //獲得索引號
         console.log(index);
-        var index2=index*3+$(this).index();
+        var index2=index*2+$(this).index();
         console.log(index2);
     
         //讓下面相應索引號item顯示 其餘隱藏
         $(".step").eq(index2).show().siblings().hide();
     })
 }
-$.fn.step = function(index){
-    $(".pic_btn").click(function(){
-        //獲得索引號
-        console.log(index);
-        var index2=index*3+$(this).index();
-        console.log(index2);
-    
-        //讓下面相應索引號item顯示 其餘隱藏
-        $(".step2").eq(index2).show().siblings().hide();
-        $(".other").css("top","500px");
-        $(".footer").css("top","500px");
-    })
-}
+
 $.fn.sub(0);
-$.fn.step(0);
 $.fn.type();
