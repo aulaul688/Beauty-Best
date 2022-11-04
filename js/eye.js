96aba03eda07b86ced155ec09f6bd46124923b6d
aulaul688 <aulaul688@gmail.com>
2022-11-04 14:20:45 +0800

    Revise footer/face img

--- js/eye.js
+++ js/eye.js
@@ -16,12 +16,19 @@ $.fn.sub = function(index){
     $(".choose_btn").click(function(){
         //獲得索引號
         var index2=index*2+$(this).index();
-        
-    
         //讓下面相應索引號item顯示 其餘隱藏
         $(".step").eq(index2).show().siblings().hide();
     })
 }
 
+$.fn.other=function(){
+    $(".o1").click(function(){
+        var index3=$(this).index()+6;
+        $(".one_type").eq(index3).show().siblings().hide();
+        $.fn.sub(index3);
+    })
+}
+
+$.fn.other();
 $.fn.sub(0);
 $.fn.type();
